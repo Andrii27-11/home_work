1398b87d082b0c2af25b9c6df3d2c4ff078b4a3a
Andrii Yuzvenko <andrii.yuzvenko@gmail.com>
2021-07-28 18:47:31 +0300

    add for and if

--- js/script.js
+++ js/script.js
@@ -10,12 +10,36 @@ const personalMovieDB = {
 	privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-	b = prompt('На сколько оцените его?', ''),
-	c = prompt('Один из последних просмотренных фильмов', ''),
-	d = prompt('На сколько оцените его?', '');
+for (let i = 0; i < 2; i++) {
+	const a = prompt('Один из последних просмотренных фильмов?', ''),
+	b = prompt('На сколько оцените его?', '');
+	if (a != '' && b != '' && a != null && b != null && a.length < 50) {
+		personalMovieDB.movies[a] = b;
+		console.log('Vse verno!');
+	} else {
+		console.log("Error");
+		i--;
+	}
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+}
+// const a = prompt('Один из последних просмотренных фильмов?', ''),
+// 	b = prompt('На сколько оцените его?', ''),
+// 	c = prompt('Один из последних просмотренных фильмов', ''),
+// 	d = prompt('На сколько оцените его?', '');
+
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
 
 console.log(personalMovieDB);
+
+if (personalMovieDB.count < 10) {
+	console.log('Priosmotreno dovolino malo filmov');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+	console.log('Vu klasicheskiy zritel');
+} else if (personalMovieDB.count >= 30) {
+	console.log('Vu kiniman');
+} else {
+	console.log('Proizishla oshubka');
+}
+
